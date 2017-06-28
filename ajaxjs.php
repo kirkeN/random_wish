d0d19e5bdf99bcfaeaee8d624be6053f17a3299f
kirkeN <kirke.narusk@gmail.com>
2017-06-28 13:46:35 +1000

    config database file

--- ajaxjs.php
+++ ajaxjs.php
@@ -1,10 +1,7 @@
 <?php
+require_once ("config.ini");
 
-$host="localhost";
-$user="test";
-$pass="t3st3r123";
-$db="test";
-$connection = mysqli_connect($host, $user, $pass, $db) or die("Cannot connect- ".mysqli_error());
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die("Cannot connect- ".mysqli_error());
 mysqli_query($connection, "SET CHARACTER SET UTF8") or die("Cannot connect to base utf-8 - ".mysqli_error($connection));
 
 $promptWish = mysqli_real_escape_string($connection,$_POST['data']);     // gets the data it received from the request
