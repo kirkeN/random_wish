5c31e9ac06470ec7ed1d61273166a4b54cd718e4
kirkeN <kirke.narusk@gmail.com>
2017-06-28 12:32:57 +1000

    minor changes

--- ajaxjs.php
+++ ajaxjs.php
@@ -1,5 +1,4 @@
 <?php
-$promptWish = $_POST['data'];     // gets the data it received from the request
 
 $host="localhost";
 $user="test";
@@ -8,6 +7,7 @@ $db="test";
 $connection = mysqli_connect($host, $user, $pass, $db) or die("Cannot connect- ".mysqli_error());
 mysqli_query($connection, "SET CHARACTER SET UTF8") or die("Cannot connect to base utf-8 - ".mysqli_error($connection));
 
+$promptWish = mysqli_real_escape_string($connection,$_POST['data']);     // gets the data it received from the request
 
 $query =mysqli_query($connection,"INSERT INTO bday_wish(wish) VALUES('$promptWish')");
 
